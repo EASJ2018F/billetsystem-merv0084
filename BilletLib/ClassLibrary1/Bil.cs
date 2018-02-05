5015996580f38b79d8f8e63177d73775941bfcab
Mervan <Merv0084@edu.easj.dk>
2018-02-05 09:49:39 +0100

    Billetsystem

---
+++ BilletLib/ClassLibrary1/Bil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BilletLib
+{
+    public class Bil
+    {
+        public string Nummerplade {get { return Nummerplade; }}
+        public DateTime Dato { get { return Dato; }}
+
+        public int Pris()
+        {
+            return 240;
+        }
+
+        public string køretøj()
+        {
+            return "Bil";
+        }
+        
+
+       
+
+
+
+    }
+   
+
+
+
+
+}
+
---
+++ BilletLib/ClassLibrary1/MC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+   public class MC
+    {
+        public string Nummerplade { get { return Nummerplade; } }
+        public DateTime Dato { get { return Dato; } }
+
+        public int Pris()
+        {
+            return 125;
+        }
+
+        public string køretøj()
+        {
+            return "MC";
+        }
+    }
+}
